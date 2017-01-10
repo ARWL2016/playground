63cd9cc1e478124fd9e65276b78f6152d2c319f1
Alistair Willis <alistairrwillis@gmail.com>
2017-01-10 22:58:59 +0900

    First contact.

--- server.js
+++ server.js
@@ -6,7 +6,19 @@ var app = express();
 
 app.get('/', function(request, response) {
     response.sendFile('public/index.html', {root: __dirname});
+    console.log(request.query); 
 });
+
+app.get('/send', function(request, response) {
+    console.log(`The first name is ${request.query.first}`); 
+    console.log(`The last name is ${request.query.last}`); 
+    response.end('thanks'); 
+});
+
+// app.post('/', function(request, response) {
+//     console.log(request.params);
+//     request.
+// });
 
 app.listen(port, function() {
     console.log("Listening on port " + port); 
